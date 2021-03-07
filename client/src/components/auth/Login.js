f79abb475c45d2d96a3a082b83de5cc9200da4b6
BayLadyCoder <rachada_32@hotmail.com>
2021-03-07 17:08:28 -0500

    Add handleChange and handleSubmit in Login and Register forms

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("Success!!");
+  };
   return (
     <section class="container">
       <div class="alert alert-danger">Invalid credentials</div>
@@ -8,22 +24,30 @@ const Login = () => {
       <p class="lead">
         <i class="fas fa-user"></i> Sign into Your Account
       </p>
-      <form class="form" action="dashboard.html">
+      <form class="form" onSubmit={(e) => handleSubmit(e)}>
         <div class="form-group">
           <input
             type="email"
             placeholder="Email Address"
             name="email"
             required
+            value={email}
+            onChange={(e) => handleChange(e)}
           />
         </div>
         <div class="form-group">
-          <input type="password" placeholder="Password" name="password" />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={(e) => handleChange(e)}
+          />
         </div>
         <input type="submit" class="btn btn-primary" value="Login" />
       </form>
       <p class="my-1">
-        Don't have an account? <a href="register.html">Sign Up</a>
+        Don't have an account? <Link to="/register">Sign Up</Link>
       </p>
     </section>
   );
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -14,12 +15,30 @@ const Register = () => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     if (password !== password2) {
       console.log("Password do not matched");
     } else {
-      console.log(formData);
+      console.log("Success!!");
+
+      //   const newUser = {
+      //     name,
+      //     email,
+      //     password,
+      //   };
+      //   try {
+      // const config = {
+      //   headers: {
+      //     "Content-Type": "application/json",
+      //   },
+      // };
+      // const body = JSON.stringify(newUser);
+      // const res = await axios.post("/api/users", body, config);
+      // console.log(res.data);
+      //   } catch (error) {
+      //     console.log(error.message);
+      //   }
     }
   };
   return (
@@ -75,7 +94,7 @@ const Register = () => {
         <input type="submit" class="btn btn-primary" value="Register" />
       </form>
       <p class="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to="/login">Sign In</Link>
       </p>
     </section>
   );
