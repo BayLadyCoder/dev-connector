e60025afdcf4f4bdb74020a4282968578544e72e
BayLadyCoder <rachada_32@hotmail.com>
2021-03-09 01:12:19 -0500

    Fix Proxy error, add Spinnner on loading, Redirect to /dashboard on Landing when user logged in

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,12 +2,35 @@ import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getCurrentProfile } from "../../actions/profile";
+import Spinner from "../layout/Spinner";
 
 const Dashboard = (props) => {
+  const { getCurrentProfile } = props;
+
   useEffect(() => {
-    props.getCurrentProfile();
-  }, []);
-  return <div>Dashboard</div>;
+    getCurrentProfile();
+  }, [getCurrentProfile]);
+
+  const { profile, loading } = props.profile;
+  const { user } = props.auth;
+
+  if (loading && profile === null) {
+    return <Spinner />;
+  }
+
+  return (
+    <React.Fragment>
+      <h1 className="large text-primary">Dashboard</h1>
+      <p className="lead">
+        <i className="fa fas-user" /> Welcome {user && user.name}
+      </p>
+      {profile !== null ? (
+        <React.Fragment>Has</React.Fragment>
+      ) : (
+        <React.Fragment>Has not</React.Fragment>
+      )}
+    </React.Fragment>
+  );
 };
 
 Dashboard.propTypes = {
--- client/src/components/layout/NavBar.js
+++ client/src/components/layout/NavBar.js
@@ -10,8 +10,14 @@ const NavBar = (props) => {
   const authLinks = (
     <ul>
       <li>
+        <Link to="/dashboard">
+          <i className="fas fa-user" />{" "}
+          <span className="hide-sm">Dashboard</span>
+        </Link>
+      </li>
+      <li>
         <a onClick={props.logoutUser} href="#!">
-          <i className="fas fa-sign-out-alt"></i>{" "}
+          <i className="fas fa-sign-out-alt" />{" "}
           <span className="hide-sm">Logout</span>
         </a>
       </li>
