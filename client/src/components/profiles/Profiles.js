0e3f91292ee31121534f076bce64a2197562e99e
BayLadyCoder <rachada_32@hotmail.com>
2021-03-11 19:32:23 -0500

    Render profiles using ProfileItem component

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -3,15 +3,39 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
 import { getProfiles } from "../../actions/profile";
+import ProfileItem from "./ProfileItem";
 
 const Profiles = (props) => {
-  const { profiles, loading } = props;
+  const { profiles, loading } = props.profile;
 
   useEffect(() => {
     props.getProfiles();
   }, []);
 
-  return <div></div>;
+  return (
+    <React.Fragment>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <React.Fragment>
+          <h1 className="large text-primary">Developers</h1>
+          <p className="lead">
+            <i className="fab fa-connectdevelop" /> Browse and connect with
+            developers
+          </p>
+          <div className="profiles">
+            {profiles.length > 0 ? (
+              profiles.map((profile) => (
+                <ProfileItem key={profile._id} profile={profile} />
+              ))
+            ) : (
+              <h4>No profiles found...</h4>
+            )}
+          </div>
+        </React.Fragment>
+      )}
+    </React.Fragment>
+  );
 };
 
 Profiles.propTypes = {
@@ -21,6 +45,7 @@ Profiles.propTypes = {
 
 const mapStateToProps = (state) => ({
   profile: state.profile,
+  getProfiles: PropTypes.func.isRequired,
 });
 
 export default connect(mapStateToProps, { getProfiles })(Profiles);
